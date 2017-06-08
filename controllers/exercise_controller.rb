8214c74d246ec5686806b41298d22b0ffb99ed4e
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-06-08 14:41:36 +0100

    The get index route is set and connect and displaying on page

--- controllers/exercise_controller.rb
+++ controllers/exercise_controller.rb
@@ -14,20 +14,77 @@ class ExercisesController < Sinatra::Base
       id: 0,
       name: "pushups",
       target_muscles: "arms,upper back, shoulders",
-      Reps: "15"
+      reps: "15"
   	},
   	{
       id: 1,
       name: "Squats",
       target_muscles: "thighs, glutes, calves",
-      Reps: "100",
+      reps: "100"
   	},
   	{
       id: 2,
       name: "situps",
-      target_muscles: "abdomen"
-      Reps: "50",
+      target_muscles: "abdomen",
+      reps: "50"
   	}]
+
+  	get '/' do
+
+      @title = " Fit Track"
+
+      @fit = $exercise
+  
+      erb :'exercises/index'
+  
+    end
+
+  	get '/:id' do
+    
+	    # get the ID and turn it in to an integer
+	    id = params[:id].to_i
+
+	    # make a single post object available in the template
+	    @fit = $exercise[id]
+	    
+	    erb :'exercises/show'
+    
+  	end
+    
+  	post '/' do
+    
+  	  "CREATE"
+
+    
+    
+  	end
+    
+    
+  	get '/new'  do
+    
+    	erb :'/exercises/new'
+    
+  	end
+    
+  	
+  	put '/:id'  do
+    
+    	"UPDATE: #{params[:id]}"
+    
+  	end
+    
+ 	delete '/:id'  do
+    
+   		 "DELETE: #{params[:id]}"
+    
+  	end
+    
+  	get '/:id/edit'  do
+    
+    	"EDIT: #{params[:id]}"
+    
+  	end
+
 
 
 
