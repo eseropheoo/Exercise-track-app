9bac42e9c3e4d3f0278d0a0f5c00d8250750405b
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-06-08 14:18:44 +0100

    created dummy  exercise array in global variable

--- controllers/exercise_controller.rb
+++ controllers/exercise_controller.rb
@@ -10,6 +10,24 @@ class ExercisesController < Sinatra::Base
       	register Sinatra::Reloader
     end
 
+    $exercise = [{
+      id: 0,
+      name: "pushups",
+      target_muscles: "arms,upper back, shoulders",
+      Reps: "15"
+  	},
+  	{
+      id: 1,
+      name: "Squats",
+      target_muscles: "thighs, glutes, calves",
+      Reps: "100",
+  	},
+  	{
+      id: 2,
+      name: "situps",
+      target_muscles: "abdomen"
+      Reps: "50",
+  	}]
 
 
 
