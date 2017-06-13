ba2158a024fe6db9172f3196d5bb80e4ecbe42ca
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-06-13 12:16:52 +0100

    changed syntax in sql command

--- db_setup.rb
+++ db_setup.rb
@@ -15,16 +15,15 @@ class DatabaseSetup
 		conn = self.open_connection
 
 		sql = "CREATE TABLE fit (
-		title TEXT NOT NULL,
-		id SERIAL NOT NULL,
-		username VARCHAR(200) NOT NULL,
-		target_muscle TEXT NOT NULL,
-		body TEXT NOT NULL,
-		days_trained SERIAL NOT NULL,
-		diet TEXT NOT NULL,
-		reps SERIAL NOT NULL,
-		image VARCHAR(100) NOT NULL,
-		PRIMARY KEY id
+			title TEXT NOT NULL,
+			id SERIAL NOT NULL,
+			username VARCHAR(200) NOT NULL,
+			target_muscle TEXT NOT NULL,
+			body TEXT NOT NULL,
+			days_trained SERIAL NOT NULL,
+			diet TEXT NOT NULL,
+			reps SERIAL NOT NULL,
+			image VARCHAR(100) NOT NULL
 		);"
 
 		conn.exec(sql)
